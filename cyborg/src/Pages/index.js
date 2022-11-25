e573d9446c1760faeab898326036802b500b329c
zana suleiman <zana.suleiman.44@gmail.com>
2022-11-25 23:36:40 +0300

    Added gh-pages

--- cyborg/src/Pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home/Home";
-export { default as Browse } from "./Browse/Browse";
-export { default as Streams } from "./Streams/Streams";
-export { default as Details } from "./Details/Details";
-export { default as Profile } from "./Profile/Profile";
--- cyborg/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Container } from './Container/Container';
-export { default as SectionWrapper } from './SectionWrapper/SectionWrapper';
-export { default as CardItem} from '../components/CardItem/CardItem';
-export { default as FeaturedGames} from '../Data/FeaturedGames/FeaturedGames';
-export { default as TopDownloadedData} from "../Data/TopDownloadedData/TopDownloadedData";
-export { default as TopDownloadedItem} from "../components/TopDownloadedItem/TopDownloadedItem";
-export { default as LiveSteamItem }from './LiveStreamItem/LiveStreamItem';
-export { default as LiveSteamItemInfo} from "../Data/LiveStreamItemData/LiveStreamItemData";
